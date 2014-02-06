73830e66eb047e0a33523c968195d867e95c462a
Steven Birkner <stevebirk36@gmail.com>
2014-02-06 14:07:56 -0500

    making qunit fail

--- js/test/test_main.js
+++ js/test/test_main.js
@@ -1,5 +1,5 @@
 test("prettydate basics", function() {
-    var now = "2008/01/28 22:25:00";
+    var now = "2008/01/28 22:24:00";
     equal(prettyDate(now, "2008/01/28 22:24:30"), "just now");
     equal(prettyDate(now, "2008/01/28 22:23:30"), "1 minute ago");
     equal(prettyDate(now, "2008/01/28 21:23:30"), "1 hour ago");
