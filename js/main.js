8bd9a9cda26d871e0105f0aa2285db5a5b13a9e5
Steven Birkner <stevebirk36@gmail.com>
2014-02-18 12:11:38 -0500

    ADD: Decorating obj w/ multi decorators

--- js/main.js
+++ js/main.js
@@ -138,6 +138,42 @@ truck.setModel("F-150");
 console.log(truck);
 
 
+function MacBook() {
+  this.cost = function () {return 997;};
+  this.screenSize = function () {return 11.6;};
+
+}
+
+function memory(macbook) {
+  var v = macbook.cost();
+  macbook.cost = function () {
+    return v + 75;
+  };
+}
+
+function engraving(macbook) {
+  var v = macbook.cost();
+  macbook.cost = function () {
+    return v + 200;
+  };
+}
+
+function insure(macbook) {
+  var v = macbook.cost();
+  macbook.cost = function () {
+    return v + 250;
+  };
+}
+
+
+var macbook = new MacBook();
+
+memory(macbook);
+engraving(macbook);
+insure(macbook);
+
+console.log(macbook.cost());
+console.log(macbook.screenSize());
 
 
 
