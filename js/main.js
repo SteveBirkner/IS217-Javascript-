20892f4136c802e4b7cacfddaebb670115e74771
Steven Birkner <stevebirk36@gmail.com>
2014-03-14 17:54:29 -0400

    MOD: cleaning things up before starting Project 1

--- js/main.js
+++ js/main.js
@@ -1,45 +1,3 @@
-var prettyDate = {
-  format: function(now, time){
-    var date = new Date(time || ""),
-      diff = (((new Date(now)).getTime() - date.getTime()) / 1000),
-      day_diff = Math.floor(diff / 86400);
-
-    if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
-      return;
-
-  return day_diff === 0 && (
-        diff < 60 && "just now" ||
-        diff < 120 && "1 minute ago" ||
-        diff < 3600 && Math.floor( diff / 60 ) +
-          " minutes ago" ||
-        diff < 7200 && "1 hour ago" ||
-        diff < 86400 && Math.floor( diff / 3600 ) +
-          " hours ago") ||
-      day_diff === 1 && "Yesterday" ||
-      day_diff < 7 && day_diff + " days ago" ||
-      day_diff < 31 && Math.ceil( day_diff / 7 ) +
-        " weeks ago";
-  },
-
-  update: function(now) {
-    var links = document.getElementsByTagName("a");
-    for ( var i = 0; i < links.length; i++ ) {
-      if ( links[i].title ) {
-        var date = prettyDate.format(now, links[i].title);
-        if ( date ) {
-          links[i].innerHTML = date;
-        }
-      }
-    }
-  }
-};
-
-function add(x,y) {
-  return x + y;
-}
-
-//Design pattern things below
-
 //constructor
 function Car(model,year,miles) {
   this.model = model;
@@ -88,8 +46,7 @@ var mySingleton = (function () {
 
     };
 
-  };
-
+  }
   return {
 
     // Get the Singleton instance if one exists
@@ -157,11 +114,3 @@ function insure(macbook) {
     return v + 250;
   };
 }
-
-
-
-
-
-
-
-
